c9c1a71de8d088f8394b76d1456d040d6c7e1338
fedful <f.fulgeri@gmail.com>
2018-04-16 06:37:36 +0200

    update

--- matlabutils.py
+++ matlabutils.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import scipy.io as spio
 
 
-dataset_path = '/home/federico/remote/datasets/RAP_annotation/RAP_annotation.mat'
+dataset_path = ''
 m = spio.loadmat(dataset_path)
 
-print(m['RAP_annotation'][0][4])
+print(m[''][0][4])
